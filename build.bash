4d0211286aa1139a2c7b426db8e90b46e99d307f
BoogieBug <boogiebug@gmail.com>
2025-12-02 11:31:51 +0700

    update build script

--- build.bash
+++ build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAGNAME=${TAGNAME:-dockers.pinacono.com/hoster}
+TAGNAME=${TAGNAME:-dockers.pinacono.com/common/hoster}
 
 docker build -t ${TAGNAME} .
-#docker push dockers.pinacono.com/hoster
+docker push dockers.pinacono.com/common/hoster
